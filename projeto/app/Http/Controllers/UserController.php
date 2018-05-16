193c9c9488127db7dfed7331ffce4ab70959ff1d
João Castelo <joaobernardoc@hotmail.com>
2018-05-16 21:03:41 +0100

    teste

--- projeto/app/Http/Controllers/UserController.php
+++ projeto/app/Http/Controllers/UserController.php
@@ -6,20 +6,5 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
-    public function index()
-    {
-
-    }
-
-    public function show($id)
-    {
-
-    }
-
-    public function update(Request $request, $id)
-    {
-
-    }
-
+    //
 }
