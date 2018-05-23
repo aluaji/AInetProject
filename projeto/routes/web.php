8b48e42175e1d74ecb7cd2834e6cf5812eb842eb
Reinaldo <rei_1989@sapo.pt>
2018-05-23 21:12:39 +0100

    Fix do Nuno

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -19,6 +19,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/me/associates', 'AssociateMembersController@viewMembersMyGroup')->name('MemberList');
 Route::get('/me/associate-of', 'AssociateMembersController@viewMembersOtherGroups')->name('OtherMemberList');
-Route::get('/accounts/{user}', '')->name('AllAccounts');
+Route::get('/accounts/{user}', 'AccountController@create')->name('AllAccounts');
 
 Route::get('/list', 'UserController@listUsers')->name('users.list')->middleware('admin');
